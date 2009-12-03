fe239c9cbbea77c6a010f29e92cd0502d7ea86a4
Makoto Yamashita <makotoy@host-12-54.dhcp.math.jussieu.fr>
2009-12-03 15:43:17 +0100

    Add Firefox plugin element

---
+++ Quicksilver/Plugins/Firefox/QSFirefoxPlugIn.h
@@ -0,0 +1,21 @@
+//
+//  QSFirefoxPlugIn.h
+//  QSFirefoxPlugIn
+//
+//  Created by Nicholas Jitkoff on 4/6/05.
+//  Copyright __MyCompanyName__ 2005. All rights reserved.
+//
+
+// Derived from Blacktree codebase
+// Makoto Yamashita 2009-11-30
+
+@interface QSFirefoxPlugIn : NSObject
+{
+}
+@end
+
+@interface QSMorkMozillaHistoryParser : QSParser
+{
+}
+@end
+
