0c475fe4534567d678612ef6a95ce2b47e308245
Makoto Yamashita <makotoy@MakoBook.local>
2009-12-05 13:44:50 +0100

    change element.xml in auto-versioning

--- put_bndl_ver.pl
+++ put_bndl_ver.pl
@@ -7,6 +7,7 @@ use strict;
 # Get the current git commit hash and use it to set the CFBundleVersion value
 my $REV = `/usr/local/git/bin/git show --abbrev-commit | grep "^commit"`;
 my $INFO = "$ENV{BUILT_PRODUCTS_DIR}/$ENV{WRAPPER_NAME}/Contents/Info.plist";
+my $ELEM = "$ENV{BUILT_PRODUCTS_DIR}/$ENV{WRAPPER_NAME}/Contents/Resources/element.xml";
 
 my $version = $REV;
 if( $version =~ /^commit\s+([^.]+)$/ )
@@ -29,4 +30,14 @@ $info =~ s/([\t ]+<key>CFBundleVersion<\/key>\n[\t ]+<string>).*?(<\/string>)/$1
 open(FH, ">$INFO") or die "$0: $INFO: $!";
 print FH $info;
 close(FH);
+
+open(FH2, "$ELEM") or die "$0: $ELEM: $!";
+my $elem = join("", <FH2>);
+close(FH2);
+ 
+$elem =~ s/([\t ]*<element [^>]+version=\").*?(\".*?>)/$1$version$2/;
+ 
+open(FH2, ">$ELEM") or die "$0: $ELEM: $!";
+print FH2 $elem;
+close(FH2);
 
