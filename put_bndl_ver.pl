c4b6347f6e94da58c61f496fe0bf71d3cb911224
Makoto Yamashita <makotoy@MakoBook.local>
2009-12-05 13:03:25 +0100

    add versioning script

---
+++ put_bndl_ver.pl
@@ -0,0 +1,32 @@
+# Xcode auto-versioning script for Subversion by Axel Andersson
+# Updated for git by Marcus S. Zarra and Matt Long
+# Modified by Makoto Yamashita
+ 
+use strict;
+ 
+# Get the current git commit hash and use it to set the CFBundleVersion value
+my $REV = `/usr/local/git/bin/git show --abbrev-commit | grep "^commit"`;
+my $INFO = "$ENV{BUILT_PRODUCTS_DIR}/$ENV{WRAPPER_NAME}/Contents/Info.plist";
+
+my $version = $REV;
+if( $version =~ /^commit\s+([^.]+)$/ )
+{ 
+	$version = $1;
+	chomp $version;
+}
+else
+{
+	$version = undef;
+}
+die "$0: No Git revision found: $REV" unless $version;
+ 
+open(FH, "$INFO") or die "$0: $INFO: $!";
+my $info = join("", <FH>);
+close(FH);
+ 
+$info =~ s/([\t ]+<key>CFBundleVersion<\/key>\n[\t ]+<string>).*?(<\/string>)/$1$version$2/;
+ 
+open(FH, ">$INFO") or die "$0: $INFO: $!";
+print FH $info;
+close(FH);
+
