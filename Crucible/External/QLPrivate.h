63e4e28592af8559ba42a688d01f6ab1ea5e2fac
Makoto Yamashita <m4k0t0y@gmail.com>
2012-11-27 18:12:03 +0900

    QuickLook became available as std api

--- Crucible/External/QLPrivate.h
+++ Crucible/External/QLPrivate.h
@@ -3,14 +3,10 @@
  *  Quicksilver01
  *
  *  Created by Nicholas Jitkoff on 6/15/07.
- *  Copyright 2007 __MyCompanyName__. All rights reserved.
  *
+ *  Derived from Blacktree codebase, Makoto Yamashita 2012
  */
 
 extern const NSString *kQLThumbnailOptionContentTypeUTI;
-//extern const NSString *kQLThumbnailOptionIconModeKey;
 
-typedef void *QLThumbnailRef;
-extern QLThumbnailRef QLThumbnailCreate(void *unknownNULL, CFURLRef fileURL, CGSize iconSize, CFDictionaryRef options);
-extern CGImageRef QLThumbnailCopyImage(QLThumbnailRef thumbnail);
 
